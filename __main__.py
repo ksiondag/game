cae42534610c13ffd582130582d3b126a33031cd
Rory Ksionda <ksiondag846@gmail.com>
2013-07-15 00:56:50 -0700

    Rendering of platforms implemented.

--- __main__.py
+++ __main__.py
@@ -3,14 +3,15 @@ import sys
 
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
+GREEN =   0, 255,   0
 
 GRAVITY = 1.0
 GROUND = 480
 WALL = 640
 
-class Character:
+class Thing:
     
-    def __init__( self, x, y ):
+    def __init__( self, x, y, width, height, color ):
         '''
         '''
         self.pos_x = x
@@ -19,16 +20,46 @@ class Character:
         self.vel_x = 0
         self.vel_y = 0
 
-        self.width  = 20
-        self.height = 20
+        self.width  = width
+        self.height = height
 
-        self.touching_ground = False
+        self.color = color
+
+    def collision_event( self, other ):
+        '''
+        '''
+        pass
+
+    def update( self ):
+        '''
+        '''
+        pass
+
+    def display( self, screen ):
+        '''
+        '''
+        pygame.draw.rect( screen, self.color, (self.pos_x, self.pos_y, 
+                                               self.width, self.height) )
+
+class Platform( Thing ):
+    pass
+
+class Character( Thing ):
+    pass
+
+class Player( Character ):
+
+    def __init__( self, *args, **kwargs ):
+        '''
+        '''
+        Character.__init__( self, *args, **kwargs )
+        self.grounded = False
 
     def move_event( self, target_location ):
         '''
         '''
 
-        if self.touching_ground:
+        if self.grounded:
             target_x, target_y = target_location
 
             self.vel_x = (target_x - self.pos_x)/15
@@ -38,6 +69,10 @@ class Character:
         else:
             pass
 
+    def collision_event( self, other ):
+        # TODO: when colliding with other object, react appropriately
+        pass
+
     def update( self ):
         '''
         '''
@@ -46,7 +81,7 @@ class Character:
 
         if self.pos_y + self.height < GROUND:
             self.vel_y += GRAVITY
-            self.touching_ground = False
+            self.grounded = False
         else:
             self.vel_x = 0
             self.vel_y = 0
@@ -56,7 +91,7 @@ class Character:
             self.tar_x = self.pos_x
             self.tar_y = self.pos_y
 
-            self.touching_ground = True
+            self.grounded = True
         
         if self.pos_x < 0:
             self.vel_x = 0
@@ -65,22 +100,18 @@ class Character:
             self.vel_x = 0
             self.pos_x = WALL - self.width
 
-    def display( self, screen ):
-        '''
-        '''
-        pygame.draw.rect( screen, WHITE, (self.pos_x, self.pos_y, 
-                                          self.width, self.height) )
 
 def main():
     '''
     '''
     pygame.init()
     screen = pygame.display.set_mode((640,480))
-    pygame.display.set_caption('Controlable Rectangle in Blank Screen')
+    pygame.display.set_caption('Controlable Rectangle in Gravity Environment')
 
     clock = pygame.time.Clock()
 
-    character = Character( 300, 200 )
+    character = Player( 300, 200, 20, 40, WHITE )
+    platforms = [Platform( 0, GROUND - 10, WALL, 10, GREEN )]
 
     while True:
         # Update clock
@@ -97,11 +128,14 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        # Move objects ...
+        # Character update and display
         character.update()
-
-        # Draw objects ...
         character.display( screen )
+
+        # Platforms update and display
+        for platform in platforms:
+            platform.update()
+            platform.display( screen )
 
         # Update the screen
         pygame.display.flip()
